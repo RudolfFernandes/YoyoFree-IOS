54102f5b0dacd34c62dc539e96c86aba94889218
Rudy <rudolf.fernandes@gmail.com>
2021-10-26 16:18:03 -0400

    Initial Commit

---
+++ YoyoFree/YoyoFreeApp.swift
@@ -0,0 +1,17 @@
+//
+//  YoyoFreeApp.swift
+//  YoyoFree
+//
+//  Created by Rudy on 2021-10-26.
+//
+
+import SwiftUI
+
+@main
+struct YoyoFreeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
